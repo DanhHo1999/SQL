80ca6e84b8e22bc1e61619e13f10c7f8261b7e0f
Danh Ho <ho.thanhdanh9400@gmail.com>
2022-06-25 11:21:55 +0700

    25/6

--- Lab1.3.sql
+++ Lab1.3.sql
@@ -1,4 +1,4 @@
-create database QL_BANHANG
+﻿create database QL_BANHANG
 use QL_BANHANG
 create table tbl_hang(
 HangID nvarchar(10) not null primary key,
@@ -31,3 +31,40 @@ alter table tbl_hangban add constraint pk_hb_hd foreign key (HoaDonID) reference
 alter table tbl_hangban add 
 constraint pk_hb_h foreign key (HangID) references tbl_hang(HangID)
 alter table tbl_hangban add constraint pc_sl check(SoLuong>0)
+
+insert into tbl_hang values ('H001','Bàn Phím','Cái',10)
+insert into tbl_hang values ('H002','Ram','Thanh',200)
+insert into tbl_hang values ('H003','Màn Hình','Màn',300)
+insert into tbl_hang values ('H004','Máy in','Cái',150)
+insert into tbl_hang values ('H005','Ổ cứng','Ổ',100)
+
+insert into tbl_khach values ('K001','Nguyễn Văn An','Hà Nội','an@gmail.com','Hinh 1')
+insert into tbl_khach values ('K002','Trần Minh Chiến','Đà nẵng','chien@gmail.com','Hinh 2')
+insert into tbl_khach values ('K003','Nguyễn Thị Hồng','Long An','hong@gmail.com','Hinh 3')
+insert into tbl_khach values ('K004','Nguyễn Thu Trang','Hồ Chí Minh','trang@gmail.com','Hinh 4')
+insert into tbl_khach values ('K005','Phạm Tuấn Ngọc','Hà Nội','ngoc@gmail.com','Hinh 5')
+insert into tbl_khach values ('K006','Phạm Thành Danh','Hồ Chí Minh','danh@gmail.com','Hinh 6')
+
+insert into tbl_hoadon values ('HD01','2021-03-12','K001')
+insert into tbl_hoadon values ('HD02','2021-04-01','K002')
+insert into tbl_hoadon values ('HD03','2021-08-09','K001')
+insert into tbl_hoadon values ('HD04','2021-12-12','K003')
+insert into tbl_hoadon values ('HD05','2022-01-12','K004')
+insert into tbl_hoadon values ('HD06','2022-01-26','K005')
+insert into tbl_hoadon values ('HD07','2022-02-02','K006')
+insert into tbl_hoadon values ('HD08','2022-03-01','K002')
+insert into tbl_hoadon values ('HD09','2022-03-11','K001')
+
+insert into tbl_hangban values ('H001','HD01',4)
+insert into tbl_hangban values ('H001','HD02',5)
+insert into tbl_hangban values ('H001','HD04',4)
+insert into tbl_hangban values ('H001','HD05',4)
+insert into tbl_hangban values ('H002','HD01',2)
+insert into tbl_hangban values ('H002','HD04',8)
+insert into tbl_hangban values ('H002','HD06',4)
+insert into tbl_hangban values ('H003','HD01',12)
+insert into tbl_hangban values ('H003','HD02',8)
+insert into tbl_hangban values ('H003','HD07',4)
+insert into tbl_hangban values ('H004','HD03',4)
+insert into tbl_hangban values ('H004','HD08',4)
+insert into tbl_hangban values ('H005','HD03',11)
--- Lab1.4.sql
+++ Lab1.4.sql
@@ -1,4 +1,4 @@
-create database QL_SINHVIEN
+﻿create database QL_SINHVIEN
 use QL_SINHVIEN
 create table tbl_monhoc
 (
@@ -23,8 +23,48 @@ create table tbl_diem
 (
 MaSV nvarchar(10)not null,
 MaMH nvarchar(10)not null,
-DiemTB int not null,
+DiemTB float not null,
 primary key (MaSV,MaMH),
 constraint fk_diem_monhoc foreign key (MaMH)references tbl_monhoc(MaMH),
 constraint fk_diem_sinhvien foreign key(MaSV)references tbl_sinhvien(MaSV)
 )
+
+insert into tbl_truong values ('MT01','ĐH Bách Khoa')
+insert into tbl_truong values ('MT02','ĐH Kinh Tế')
+insert into tbl_truong values ('MT03','ĐH Sư Phạm')
+insert into tbl_truong values ('MT04','ĐH Giao Thông')
+insert into tbl_truong values ('MT05','VTCA')
+
+insert into tbl_sinhvien values ('SV001','Nguyễn Văn Hùng','Nam','1990-03-12','MT01')
+insert into tbl_sinhvien values ('SV002','Trần Diệu Nhi','Nữ','1990-06-22','MT02')
+insert into tbl_sinhvien values ('SV003','Phạm Minh Đăng','Nam','1990-12-09','MT03')
+insert into tbl_sinhvien values ('SV004','Nguyễn Anh Đào','Nữ','1991-08-12','MT01')
+insert into tbl_sinhvien values ('SV005','Trần Văn Hưng','Nam','1990-09-12','MT02')
+
+insert into tbl_monhoc values ('MH01','Thiết kế WEB')
+insert into tbl_monhoc values ('MH02','Lập Trình WEB')
+insert into tbl_monhoc values ('MH03','ASP.NET')
+insert into tbl_monhoc values ('MH04','SQL Server')
+insert into tbl_monhoc values ('MH05','HTML và CSS')
+insert into tbl_monhoc values ('MH06','Lập trình căn bản')
+
+insert into tbl_diem values ('SV001','MH01',9)
+insert into tbl_diem values ('SV001','MH02',6.5)
+insert into tbl_diem values ('SV001','MH03',8.5)
+insert into tbl_diem values ('SV001','MH04',7)
+insert into tbl_diem values ('SV002','MH01',8)
+insert into tbl_diem values ('SV002','MH02',6.8)
+insert into tbl_diem values ('SV002','MH03',7.5)
+insert into tbl_diem values ('SV002','MH04',9)
+insert into tbl_diem values ('SV003','MH01',8)
+insert into tbl_diem values ('SV003','MH02',8)
+insert into tbl_diem values ('SV003','MH03',5)
+insert into tbl_diem values ('SV003','MH04',8)
+insert into tbl_diem values ('SV004','MH01',8.5)
+insert into tbl_diem values ('SV004','MH02',8.6)
+insert into tbl_diem values ('SV004','MH03',5.8)
+insert into tbl_diem values ('SV004','MH04',8.5)
+insert into tbl_diem values ('SV005','MH01',7)
+insert into tbl_diem values ('SV005','MH02',9.6)
+insert into tbl_diem values ('SV005','MH03',8.8)
+insert into tbl_diem values ('SV005','MH04',8.5)
